abbc9550a7b56efea649e7fe021d4184d3da88d0
Michael Figueroa <mafigueroa1@buffs.wtamu.edu>
2019-04-05 15:13:39 -0500

    inital commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Models;
+
+namespace Stripe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Charge(string  stripeEmail, string stripeToken)
+{
+    var customers = new CustomerService();
+    var charges = new ChargeService();
+
+    var customer = customers.Create(new CustomerCreateOptions {
+      Email = stripeEmail,
+      SourceToken = stripeToken
+    });
+
+    var charge = charges.Create(new ChargeCreateOptions {
+      Amount = 500,
+      Description = "Sample Charge",
+      Currency = "usd",
+      CustomerId = customer.Id
+    });
+
+
+    return View();
+}
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
